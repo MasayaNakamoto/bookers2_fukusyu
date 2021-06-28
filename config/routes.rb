366c2193c597aa05da80f5c1feaacc4634826015
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-28 10:34:23 +0000

    fukusyu-stage1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/destroy'
   devise_for :users
    root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

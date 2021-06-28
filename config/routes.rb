f9be03b9bc30cd777c7e23b6bd54ee76e76cf7a8
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-06-28 10:51:20 +0000

    fukusyu-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/destroy'
-  devise_for :users
-   root to: 'homes#top'
+     resources :users
+     resources :books
+     devise_for :users
+     root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
